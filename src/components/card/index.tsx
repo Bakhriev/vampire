4e8eee19fc08085dd31da89d3cd58faa9ed3ede8
Bakhriev <muradbakhriev@gmail.com>
2024-12-02 23:52:56 +0500

    cancel img

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React, { FC } from "react";
 
 interface ICard {
@@ -9,7 +10,13 @@ interface ICard {
 export const Card: FC<ICard> = ({ imageUrl, imageAlt, title }) => {
   return (
     <div className="card">
-      <img className="card__img" src={imageUrl} alt={imageAlt || ""} />
+      <Image
+        className="card__img"
+        src={imageUrl}
+        alt={imageAlt || ""}
+        width={450}
+        height={210}
+      />
       <div className="card__title">{title}</div>
     </div>
   );
